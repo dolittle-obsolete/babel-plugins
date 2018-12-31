d6a5c5176a36ca7766b4e01d3a7cc11b490975ef
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-31 16:30:06 +0100

    Up and running

--- Source/testapp/index.js
+++ Source/testapp/index.js
@@ -1,4 +1,1 @@
-
-export class myClass {
-        
-}
+foo === bar;
---
+++ Source/transform-no-prototype-alias/index.js
@@ -0,0 +1,15 @@
+export default function ({ types: t }) {
+    return {
+        visitor: {
+            
+            BinaryExpression(path, state) {
+                if (path.node.operator !== "===") {
+                    return;
+                }
+
+                path.node.left = t.identifier("sebmck");
+                path.node.right = t.identifier("dork");                
+            }
+        }
+    }
+}
