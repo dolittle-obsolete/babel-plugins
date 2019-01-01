9d7980d83c67ba300ddc3919f1f8a93f3bce35c6
Einar Ingebrigtsen <einar@dolittle.com>
2019-01-01 20:46:55 +0100

    Up and running with the no-prototype-alias plugin

--- Source/transform-no-prototype-alias/index.js
+++ Source/transform-no-prototype-alias/index.js
@@ -1,113 +1,36 @@
-// https://shuheikagawa.com/blog/2015/09/13/lets-create-a-babel-plugin/
-// https://github.com/babel/babel/blob/master/packages/babel-types/src/definitions/core.js
-// https://github.com/babel/babel/blob/master/packages/babel-types/src/definitions/es2015.js
-// https://github.com/jsdoc2md/jsdoc-parse
+export default function ({ types: t }) {
+    return {
+        visitor: {
+            ClassDeclaration(path) {
+            },
+            VariableDeclaration(path) {
+                let declarations = path.node.declarations.filter(_ => _.id.name.indexOf('_proto') >= 0);
+                if (declarations && declarations.length > 0) {
+                    path.remove();
+                }
+            },
 
-import doctrine from 'doctrine';
-import traverse from '@babel/traverse';
+            ExpressionStatement(path) {
 
-let nothing = {}; //doctrine.parse("/** */", { unwrap: true });
+                let expression = path.node.expression;
+                if (expression.operator == '=' &&
+                    expression.type == 'AssignmentExpression' &&
+                    expression.left.type == 'MemberExpression' &&
+                    expression.left.object.name &&
+                    expression.left.object.name.indexOf('_proto') >= 0) {
 
-
-function getJsDocFrom(path) {
-
-    try {
-        if (path.node.leadingComments != null) {
-
-            path.node.leadingComments.forEach(comment => {
-                let value = comment.value;
-                if (value.indexOf('//') >= 0) return;
-                if (value.indexOf('*') == 0) {
-                    value = `/*${value} */`;
                     try {
-                        let result = doctrine.parse(value, { unwrap: true });
-                        console.log(JSON.stringify(result));
-                        return result;
+
+                        let functionParent = path.getFunctionParent();
+                        let className = functionParent.node.body.body[0].id.name;
+                        let methodName = expression.right.id.name;
+                        expression.left = t.identifier(`${className}.prototype.${methodName}`);
                     } catch (e) {
                         console.log(e);
                     }
                 }
-            });
-        }
-    } catch (e) {
-        console.log(e);
-    }
-
-
-    return nothing;
-}
-
-
-export default function ({ types: t }) {
-    return {
-        visitor: {
-            Program(
-                path,
-                {
-                    file: {
-                        ast: { comments },
-                    },
-                    opts,
-                },
-            ) {
-            },
-            Identifier() {
-
-            },
-            BlockStatement() {
-
-            },
-            ReturnStatement() {
-
-            },
-            FunctionDeclaration() {
-
-            },
-            FunctionExpression() {
-
-            },
-            ArrowFunctionExpression() {
-
-            },
-            ExportDeclaration(path) {
-                getJsDocFrom(path);
-            },
-            Class(path) {
-                getJsDocFrom(path);
-            },
-            ClassDeclaration(path) {
-
-            },
-            ClassMethod(path) {
-                console.log(JSON.stringify(path.node));
-                getJsDocFrom(path);
-            },
-            ClassProperty(path) {
-                getJsDocFrom(path);
-            },
-            ObjectMethod() {
-                console.log(JSON.stringify(path.node));
-                getJsDocFrom(path);
-            },
-            FunctionDeclaration(path) {
-                getJsDocFrom(path);
-            },
-            FunctionExpression(path) {
-                //console.log(JSON.stringify(path.node));
-                getJsDocFrom(path);
-            },
-            MemberExpression() {
-
-            },
-
-            BinaryExpression(path, state) {
-                if (path.node.operator !== "===") {
-                    return;
-                }
-
-                path.node.left = t.identifier("sebmck");
-                path.node.right = t.identifier("dork");
             }
+
         }
     }
 }
